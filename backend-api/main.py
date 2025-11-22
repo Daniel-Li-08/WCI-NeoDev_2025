4ca07c57543c2d8a355620fd61a06969593b7957
Camleaf <bdaace09@gmail.com>
2025-11-22 13:03:23 -0500

    spjfpods

--- backend-api/main.py
+++ backend-api/main.py
@@ -1,10 +1,11 @@
 from flaskr import create_app
+from flask import Flask
 
-def main():
+def main(app:Flask):
 
-    app = create_app()
     app.run(host="0.0.0.0", port=5000, debug=True)
 
 
 if __name__ == "__main__":
-    main()
+    app = create_app()
+    main(app)
