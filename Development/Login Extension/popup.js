53b7694d75a9321753f0239eb85ad8eb30235d9d
Camleaf <bdaace09@gmail.com>
2025-11-23 10:16:22 -0500

    scaffold cart scraper

--- Development/Login Extension/popup.js
+++ Development/Login Extension/popup.js
@@ -16,33 +16,33 @@ const getKey = async (key) => {
 
 const loadnames = async () => {
         // Store the result  
-//   const nm = await getKey("name");
-//   let pw = await getKey("pw");
-//   let prime = await getKey("prime");
-    // check if stored in extension storage first
-    const nm = await localStorage.getItem("name");
-    const pw = await localStorage.getItem("name");
-    const prime = await localStorage.getItem("name");
+        nm = await getKey("name");
+        pw = await getKey("pw");
+        prime = await getKey("prime");
 
     if (nm == null || pw == null || prime == null){
-
-        // get from page
-        const nm = await getKey("name");
-        const pw = await getKey("pw");
-        const prime = await getKey("prime");
+        
+        nm = await localStorage.getItem("name");
+        pw = await localStorage.getItem("pw");
+        prime = await localStorage.getItem("prime");
 
         if (nm == null || pw == null || prime == null){
             // Move to other page if not currently on our site
             chrome.tabs.create({ url: "https://wci-neo-dev-2025.vercel.app/login"});
-        } else {
-
-            localStorage.setItem("name",nm)
-            localStorage.setItem("pw",pw)
-            localStorage.setItem("prime",prime)
+            return
         }
+    } else {
+
+        localStorage.setItem("name",nm)
+        localStorage.setItem("pw",pw)
+        localStorage.setItem("prime",prime)
     }
 
-    
+    if (prime == "true"){
+        window.location = chrome.runtime.getURL("./adder/adder.html")
+    } else {
+        window.location = chrome.runtime.getURL("./scraper/scraper.html")
+    }
 
 }
 
